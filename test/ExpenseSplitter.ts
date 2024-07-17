6c70a82358960637041b71781d5dd1d5f393c1bb
ned3144 <nadav.misgav@gmail.com>
2024-07-17 18:52:38 +0300

    test: add create expense test

--- test/ExpenseSplitter.ts
+++ test/ExpenseSplitter.ts
@@ -10,23 +10,42 @@ describe("ExpenseSplitter", function () {
     // and reset Hardhat Network to that snapshot in every test.
     async function deployFixture() {
         // Contracts are deployed using the first signer/account by default
-        const [owner, otherAccount] = await hre.ethers.getSigners();
+        const [owner, user1, user2, user3] = await hre.ethers.getSigners();
 
         const ExpenseSplitter = await hre.ethers.getContractFactory("ExpenseSplitter");
-        const expense_splitter = await ExpenseSplitter.deploy();
+        const expenseSplitter = await ExpenseSplitter.deploy();
 
-        return { expense_splitter, owner, otherAccount };
+        return { expenseSplitter, owner, user1, user2, user3 };
     }
 
 
     describe("Expenses", function () {
-        describe("Sanity", function () {
+        describe("Sanity", async function () {
             it("Should return the current expenses", async function () {
-                const { expense_splitter } = await loadFixture(deployFixture);
+                const { expenseSplitter } = await loadFixture(deployFixture);
 
-                expect(expense_splitter.expenses.length).to.eq(0,
+                expect(expenseSplitter.expenses.length).to.eq(0,
                     "There are some expenses that were not expected"
                 );
+            });
+
+            it("Create an expense", async function () {
+                const { expenseSplitter, owner, user1, user2, user3 } = await loadFixture(deployFixture);
+                const amount = 9
+                const splitAddresses = [user1.address, user2.address, user3.address]
+
+                await expect(expenseSplitter.createExpense(amount, splitAddresses))
+                    .to.emit(expenseSplitter, "LogExpenseCreated")
+                    .withArgs(0, owner.address, amount, splitAddresses);
+
+                const expense = await expenseSplitter.getExpense(0);
+
+                expect(expense[0]).to.equal(owner.address); // creator
+                expect(expense[1]).to.equal(amount); // amount
+                expect(expense[2]).to.deep.equal(splitAddresses); // splitAddresses
+                expect(expense[3]).to.be.false; // completed
+                expect(expense[4]).to.deep.equal([false, false, false]); // approvals
+                expect(expense[5]).to.equal(0); // collectedAmount
             });
         });
     });
