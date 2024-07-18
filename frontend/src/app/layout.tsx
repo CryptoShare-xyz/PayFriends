f0a6a9e929440b00057a0c577dd4f232d4865ffa
ned3144 <nadav.misgav@gmail.com>
2024-07-18 11:47:24 +0300

    feat(wp): hero

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,10 @@
+
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+
+
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -22,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen bg-background font-sans antialiased bg-slate-200",
           fontSans.variable
         )}
       >{children}</body>
