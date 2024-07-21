12c9254edeeb85496f07c38803e3f382e089b77c
ned3144 <nadav.misgav@gmail.com>
2024-07-21 20:21:30 +0300

    tests: allow local testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import { HardhatUserConfig, vars } from "hardhat/config";
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and add it to the configuration variables
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY", "");
 
 // Add your Sepolia account private key to the configuration variables
 // To export your private key from Coinbase Wallet, go to
@@ -11,11 +11,14 @@ const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY", "");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  // This makes sure tests are ran locally
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {},
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY],
