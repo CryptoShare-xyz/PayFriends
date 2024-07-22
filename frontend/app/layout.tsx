11e1d8172d5fb584097cf6742ddd712a01a24516
ned3144 <nadav.misgav@gmail.com>
2024-07-23 00:24:30 +0300

    📂 chore: refactor frontend dir to `create-web3-dapp`

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { Inter as FontSans } from "next/font/google";
+import { WagmiConfig, createConfig } from "wagmi";
+import "./globals.css";
+
+const config = createConfig(
+  getDefaultConfig({
+    alchemyId: process.env.ALCHEMY_API_KEY,
+    walletConnectProjectId: process.env.WALLET_CONNECT_PROJECT_ID || "",
+    appName: "CryptoShare",
+  })
+);
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <WagmiConfig config={config}>
+        <ConnectKitProvider theme="auto" mode="light">
+          <body
+            className={cn(
+              "min-h-screen bg-background font-sans antialiased bg-slate-200",
+              fontSans.variable
+            )}
+          >
+            {children}
+          </body>
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </html>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,7 @@
 'use client'
 
-import { getPublicClient } from '@wagmi/core';
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { useReadContract } from 'wagmi';
-import ExpenseContract from "./ExpenseSplitter.json";
-import { formatAddress } from "./utils";
 
 import {
   Table,
@@ -16,13 +13,9 @@ import {
   TableRow
 } from "@/components/ui/table";
 import Link from "next/link";
+import { useState } from "react";
 
-import { config } from "@/config";
-import { useEffect, useState } from "react";
-import { ExpenseModal } from "./ExpenseModal";
-
-const contractAddress = "0xF5e2a7e572094b035bfC1E6070ee98fB5Eb79a21";
-const publicClient = getPublicClient(config)
+import { formatAddress } from "@/lib/utils";
 
 type Expense = {
   id: string,
@@ -30,66 +23,13 @@ type Expense = {
   from: string
 }
 
+const contractAddress = "0xF5e2a7e572094b035bfC1E6070ee98fB5Eb79a21";
+
 export default function Home() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
-  const totalExpenses = useReadContract({
-    abi: ExpenseContract.abi,
-    address: contractAddress,
-    functionName: 'getExpensesLength'
-  })
-
-  const cashFlow = useReadContract({
-    abi: ExpenseContract.abi,
-    address: contractAddress,
-    functionName: 'cashFlow'
-  })
-
-  const activeExpenses = useReadContract({
-    abi: ExpenseContract.abi,
-    address: contractAddress,
-    functionName: 'activeExpenses'
-  })
-
-  const getExpenses = async () => {
-    if (!publicClient) {
-      console.error("publicClient is null")
-      return []
-    }
-
-    const logs = await publicClient.getContractEvents({
-      address: contractAddress,
-      abi: ExpenseContract.abi,
-      eventName: "LogExpenseCreated",
-      // TODO: how to know from which block ?
-      fromBlock: 6333558n,
-    })
-
-    const fetchedExpenses = logs.map(log => (
-      {
-        id: log.args.address,
-        from: log.args.creator,
-        amount: Number.parseInt(log.args.amount)
-      }
-    ));
-
-    setExpenses(fetchedExpenses)
-
-  }
-  useEffect(() => {
-    getExpenses()
-  }, [])
-
-  // useWatchContractEvent({
-  //   address: contractAddress,
-  //   abi: ExpenseContract.abi,
-  //   eventName: 'LogExpenseCreated',
-  //   onLogs(logs) {
-  //     console.log('New logs!', logs)
-  //   },
-  // })
 
   return (
-    <div className="lg:max-w-[80%] mx-auto">
+    <main className="lg:max-w-[80%] mx-auto">
       <section id="hero" className="bg-slate-700 px-4 py-4 lg:rounded-2xl">
         {/* <nav className="flex mb-4 lg:mb-16">
           <Button />
@@ -98,7 +38,7 @@ export default function Home() {
           <div className="my-auto">
             <h1 className="text-slate-50 font-extrabold tracking-[6px] lg:text-6xl text-4xl my-2">CryptoShare</h1>
             <h2 className="text-slate-400 my-2 w-[80%] mx-auto ">A group payments app to split different payments among friends</h2>
-            <ExpenseModal createExpense={() => alert('unimplemented')} />
+            <Button>Create expense </Button>
           </div>
           <aside className="w-[80%] max-w-[480px] my-2 mx-auto">
             <Image src="/hero.svg" width={640} height={640} alt=" hero" />
@@ -106,27 +46,23 @@ export default function Home() {
         </div>
       </section>
 
-
       <div className="lg:max-w-[80%] bg-slate-100 mx-auto min-h-screen lg:rounded-2xl lg:-mt-24 py-4">
-
         <section id="stats" className="flex flex-col text-center lg:flex-row justify-evenly py-8 mx-auto gap-4 ">
           <article className="text-slate-700 lg:text-2xl text-xl" >
             <h1 className="uppercase ">cash flow</h1>
-            <small>{cashFlow.isSuccess ? Number.parseInt(cashFlow.data as string) : 0} ETH</small>
+            <small>{0} ETH</small>
           </article>
           <article className="text-slate-700 lg:text-2xl text-xl" >
             <h1 className="uppercase">total expenses</h1>
-            <small>{totalExpenses.isSuccess ? Number.parseInt(totalExpenses.data as string) : 0}</small>
+            <small>{0}</small>
           </article>
           <article className="text-slate-700 lg:text-2xl text-xl" >
             <h1 className="uppercase">active expenses</h1>
-            <small>{activeExpenses.isSuccess ? Number.parseInt(activeExpenses.data as string) : 0}</small>
+            <small>{0}</small>
           </article>
         </section>
 
-
         <hr className="w-[90%] mx-auto bg-slate-200 h-[2px] my-8" />
-
 
         <section id="Expenses" className="py-8 w-[90%] mx-auto max-w-[48rem]">
           <Table>
@@ -147,7 +83,8 @@ export default function Home() {
             </TableBody>
           </Table>
         </section>
+
       </div>
-    </div >
-  )
+    </main>
+  );
 }
