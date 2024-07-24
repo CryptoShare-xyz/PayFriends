65992620255d8f6f79052c1fff08e8a66ad65ad0
ned3144 <nadav.misgav@gmail.com>
2024-07-24 20:39:50 +0300

    feat: add group routes

---
+++ frontend/app/dashboard/layout.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { cn } from "@/lib/utils";
+import { ConnectKitButton } from "connectkit";
+import Link from "next/link";
+import { useMediaQuery } from 'react-responsive';
+
+
+function MainNav({
+    className,
+    ...props
+}: React.HTMLAttributes<HTMLElement>) {
+    return (
+        <nav
+            className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+            {...props}
+        >
+            <Link
+                href="/dashboard"
+                className="text-sm  transition-colors hover:text-primary text-[#6c63ff] font-bold"
+            >
+                Overview
+            </Link>
+            <Link
+                href="/dashboard"
+                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+            >
+                Settings
+            </Link>
+        </nav>
+    )
+}
+
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const notMobile = useMediaQuery({
+        query: '(min-width: 640px)'
+    })
+    return (
+        <div className="flex-col md:flex px-8 lg:max-w-[70%] mx-auto bg-slate-100 min-h-screen lg:rounded-2xl" >
+            <div className="border-b">
+                <div className="flex h-16 items-center px-4">
+                    <MainNav className="mx-6" />
+                    <div className="ml-auto flex items-center space-x-4">
+                        <ConnectKitButton showBalance={notMobile} />
+                    </div>
+                </div>
+            </div>
+            {children}
+        </div >
+    )
+}
--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -12,9 +12,9 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
-import { ConnectKitButton } from "connectkit"
+
 import { redirect } from 'next/navigation'
-import { useMediaQuery } from 'react-responsive'
+
 
 import Link from "next/link"
 
@@ -31,7 +31,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { cn, formatAddress } from "@/lib/utils"
+import { formatAddress } from "@/lib/utils"
 import { Activity } from 'lucide-react'
 import { useEffect, useState } from "react"
 import { useAccount } from 'wagmi'
@@ -108,30 +108,7 @@ function CreateGroupDialog() {
 }
 
 
-function MainNav({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLElement>) {
-  return (
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-      {...props}
-    >
-      <Link
-        href="/dashboard"
-        className="text-sm  transition-colors hover:text-primary text-[#6c63ff] font-bold"
-      >
-        Overview
-      </Link>
-      <Link
-        href="/dashboard"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Settings
-      </Link>
-    </nav>
-  )
-}
+
 
 const ownedGroups = [
   {
@@ -181,29 +158,27 @@ const involvedGroups = [
   },
 ]
 
-const GroupCard: React.FC<typeof ownedGroups[number]> = ({ name, description, amount }) => {
+const GroupCard: React.FC<typeof ownedGroups[number]> = ({ id, name, description, amount }) => {
   return (
-    <Card className="md:w-[12rem]">
-      < CardHeader className="" >
-        <CardTitle className="mb-2 capitalize">
-          {name}
-        </CardTitle>
-        <span className="text-slate-500 text-sm">{description}</span>
-      </CardHeader >
-      <CardContent>
-        <span className="flex text-md text-slate-800">
-          Collected <span className="ml-auto">{amount}</span>
-        </span>
-      </CardContent>
-    </Card>
+    <Link href={`/dashboard/group/${id}`}>
+      <Card className="md:w-[12rem] hover:scale-105 hover:border-2 hover:border-[#6b63ffa1] focus:scale-105 focus:border-2 focus:border-[#6b63ffa1]">
+        <CardHeader>
+          <CardTitle className="mb-2 capitalize">
+            {name}
+          </CardTitle>
+          <span className="text-slate-500 text-sm">{description}</span>
+        </CardHeader >
+        <CardContent>
+          <span className="flex text-md text-slate-800">
+            Collected <span className="ml-auto">{amount}</span>
+          </span>
+        </CardContent>
+      </Card>
+    </Link>
   )
 }
 
 export default function DashboardPage() {
-  const notMobile = useMediaQuery({
-    query: '(min-width: 640px)'
-  })
-
   const { isConnected } = useAccount();
   const [mounted, setMounted] = useState<Boolean>(false);
 
@@ -220,73 +195,61 @@ export default function DashboardPage() {
   }
 
   return (
-    <>
-      <div className="flex-col md:flex px-8 lg:max-w-[70%] mx-auto bg-slate-100 min-h-screen lg:rounded-2xl">
-        <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-              <ConnectKitButton showBalance={notMobile} />
-            </div>
-          </div>
+    <div className="p-8 pt-6">
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <div className="flex items-center space-x-2">
+          <CreateGroupDialog />
         </div>
-        <div className="p-8 pt-6">
-          <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <div className="flex items-center space-x-2">
-              <CreateGroupDialog />
+      </div>
+      <div className="flex flex-wrap">
+        <Tabs defaultValue="owned" className="space-y-4 xl:w-3/5 mb-4">
+          <TabsList>
+            <TabsTrigger value="owned">Owned groups</TabsTrigger>
+            <TabsTrigger value="involved">
+              Involved groups
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="owned" className="space-y-4">
+            <div className="flex flex-wrap gap-4">
+              {
+                ownedGroups.map(group => <GroupCard key={group.id} {...group} />)
+              }
             </div>
+          </TabsContent>
+          <TabsContent value="involved" className="space-y-4">
+            <div className="flex flex-wrap gap-4">
+              {
+                involvedGroups.map(group => <GroupCard key={group.id} {...group} />)
+              }
+            </div>
+          </TabsContent>
+        </Tabs>
+        <aside className="xl:w-2/5 w-full mt-2">
+          <div className="flex p-1 mb-4">
+            <h1 className="font-semibold text-md">Recent activity</h1>
+            <small className="ml-auto"><Activity size={16} className="text-muted-foreground" /></small>
           </div>
-          <div className="flex flex-wrap">
-            <Tabs defaultValue="owned" className="space-y-4 xl:w-3/5 mb-4">
-              <TabsList>
-                <TabsTrigger value="owned">Owned groups</TabsTrigger>
-                <TabsTrigger value="involved">
-                  Involved groups
-                </TabsTrigger>
-              </TabsList>
-              <TabsContent value="owned" className="space-y-4">
-                <div className="flex flex-wrap gap-4">
-                  {
-                    ownedGroups.map(group => <GroupCard key={group.id} {...group} />)
-                  }
-                </div>
-              </TabsContent>
-              <TabsContent value="involved" className="space-y-4">
-                <div className="flex flex-wrap gap-4">
-                  {
-                    involvedGroups.map(group => <GroupCard key={group.id} {...group} />)
-                  }
-                </div>
-              </TabsContent>
-            </Tabs>
-            <aside className="xl:w-2/5 w-full mt-2">
-              <div className="flex p-1 mb-4">
-                <h1 className="font-semibold text-md">Recent activity</h1>
-                <small className="ml-auto"><Activity size={16} className="text-muted-foreground" /></small>
-              </div>
-              <Table className="bg-white border border-separate rounded-xl">
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Address</TableHead>
-                    <TableHead>Transaction</TableHead>
-                    <TableHead>Amount</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {events.map(({ address, transaction, amount }) => (
-                    <TableRow key={address}>
-                      <TableCell className="font-medium">{formatAddress(address)}</TableCell>
-                      <TableCell className="font-medium uppercase">{transaction}</TableCell>
-                      <TableCell>{amount} ETH</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </aside>
-          </div>
-        </div>
-      </div >
-    </>
+          <Table className="bg-white border border-separate rounded-xl">
+            <TableHeader>
+              <TableRow>
+                <TableHead>Address</TableHead>
+                <TableHead>Transaction</TableHead>
+                <TableHead>Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {events.map(({ address, transaction, amount }) => (
+                <TableRow key={address}>
+                  <TableCell className="font-medium">{formatAddress(address)}</TableCell>
+                  <TableCell className="font-medium uppercase">{transaction}</TableCell>
+                  <TableCell>{amount} ETH</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </aside>
+      </div>
+    </div>
   )
 }
