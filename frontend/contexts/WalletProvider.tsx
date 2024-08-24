062093a59c1c6cfeea4eeca62ef30417e97b56ff
ned3144 <nadav.misgav@gmail.com>
2024-08-24 17:33:10 +0300

    feat: move to base-sepolia

--- frontend/contexts/WalletProvider.tsx
+++ frontend/contexts/WalletProvider.tsx
@@ -1,29 +1,48 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React, { ReactNode } from "react";
-import { WagmiProvider, createConfig } from "wagmi";
-import { hardhat, baseSepolia } from "wagmi/chains";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { baseSepolia, hardhat } from "wagmi/chains";
 
 const isDevelopment = process.env.NODE_ENV === 'development';
-const selectedChains = isDevelopment ? [hardhat] : [baseSepolia];
+if (isDevelopment) {
+    var config = createConfig(
+        getDefaultConfig({
+            chains: [hardhat],
+            transports: {
+                [hardhat.id]: http(
+                    "http://127.0.0.1:8545",
+                ),
+            },
+            walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
+            appName: "CryptoShareDEV",
+        })
+    );
+} else {
+    var config = createConfig(
+        getDefaultConfig({
+            chains: [baseSepolia],
+            transports: {
+                [baseSepolia.id]: http(
+                    `https://base-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+                ),
+            },
+            walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
+            appName: "CryptoShare",
+        })
+    );
+}
+
 
 const queryClient = new QueryClient()
 
-const config = createConfig(
-    getDefaultConfig({
-        chains: selectedChains,
-        alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
-        walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
-        appName: "CryptoShare",
-    })
-);
 
 export const WalletProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     return (
         <WagmiProvider config={config}>
             <QueryClientProvider client={queryClient}>
-            <ConnectKitProvider theme="auto" mode="light">
-                {children}
+                <ConnectKitProvider theme="auto" mode="light">
+                    {children}
                 </ConnectKitProvider>
             </QueryClientProvider>
         </WagmiProvider>
