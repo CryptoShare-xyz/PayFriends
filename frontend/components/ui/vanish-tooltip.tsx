172f41d4229b380cc8bf7fd0595d932926598a6b
ned3144 <nadav.misgav@gmail.com>
2024-10-04 22:27:29 +0300

    feat: add copied tooltip

---
+++ frontend/components/ui/vanish-tooltip.tsx
@@ -0,0 +1,36 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import React, { useEffect, useState } from "react"
+
+export function VanishTooltip({ content, children, onClick, timeout = 1000 }: { content: string, children: React.ReactElement, onClick?: () => void, timeout?: number }) {
+    const [showTooltip, setShowTooltip] = useState(false)
+
+    useEffect(() => {
+        let timer: NodeJS.Timeout
+        if (showTooltip) {
+            timer = setTimeout(() => {
+                setShowTooltip(false)
+            }, timeout)
+        }
+        onClick && onClick();
+        return () => clearTimeout(timer)
+    }, [showTooltip, timeout, onClick])
+
+    const handleClick = () => {
+        setShowTooltip(true)
+    }
+
+    const Trigger = React.cloneElement(children, { onClick: handleClick });
+
+    return (
+        <TooltipProvider>
+            <Tooltip open={showTooltip}>
+                <TooltipTrigger asChild>
+                    {Trigger}
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p className="z-50">{content}</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+}
